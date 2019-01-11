8b0083769b8056888ee547eb9e9c078f8ee80727
Jennifer Ferreira <jennifer.melo@ctec.ufal.br>
2019-01-11 09:51:17 -0200

    Adicionando essa branch

---
+++ Primeiro.py
@@ -0,0 +1,15 @@
+lista=[1,2,3]
+dicionario={'nome':'Pedro','idade':22}
+print(dicionario)
+
+a=10
+b=5.2
+print(a+b)
+a='Agora sou uma string'
+print(a)
+print(b)
+#oi
+#comentarios 
+""" 
+oi tudo bom com voce
+"""
